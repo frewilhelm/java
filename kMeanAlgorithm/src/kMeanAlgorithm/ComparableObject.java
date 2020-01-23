6d699211568bf2d81f88969fff51f49f8515bbd4
fwilhelm92 <44143442+fwilhelm92@users.noreply.github.com>
2020-01-23 16:37:26 +0100

    add interface for comparable objects (datapoints and centroids

---
+++ kMeanAlgorithm/src/kMeanAlgorithm/ComparableObject.java
@@ -0,0 +1,26 @@
+package kMeanAlgorithm;
+
+/**
+ * An interface of the datastructure of comparable objects.
+ *  Necessary to compare datapoints and centroids
+ * @author fwilhelm92
+ * @IDE Eclipse 2018-09 (4.9.0)
+ * @version Java 1.8.0_191-b12
+ *
+ */
+public interface ComparableObject {
+
+	/**
+	 * By this method, one gets the centroid-dimensions of a respective datapoint
+	 * @return centroid
+	 */
+	public InterfaceCentroid getCentroid();
+	
+	/**
+	 * This method returns the values of a centroid or a datapoint, that are stored in an array.
+	 *  Depeding on the given input (see Project.java) there can be several dimensions
+	 * @return double[]
+	 */
+	double[] getDimensions();
+	
+}
---
+++ kMeanAlgorithm/src/kMeanAlgorithm/InterfaceCentroid.java
@@ -0,0 +1,5 @@
+package kMeanAlgorithm;
+
+public interface InterfaceCentroid {
+
+}
